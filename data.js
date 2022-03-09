206e3a92b21c193759dd58b292de53e25fca379f
Rex Reed <ReedRexC@JohnDeere.com>
2022-03-08 20:12:20 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+data = {
+    subs: {
+        "TyranasaurusDad": 69,
+        "aronicco":1
+    },
+    gifts: {
+        "TyranasaurusDad": 420,
+        "aronicco":1
+    },
+    bits: {
+        "TyranasaurusDad": 69420,
+        "aronicco":1
+    }
+}
